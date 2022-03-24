6ef5e5cc8a1e4b9769cc615467d7b4cc3a6c459b
s-watanabe <swata82@gmail.com>
2022-03-24 23:53:58 +0900

    WIP test

--- laravel-app/resources/views/schedule/index.blade.php
+++ laravel-app/resources/views/schedule/index.blade.php
@@ -3,8 +3,28 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
+    <div id='calendar'></div>
 
 </div>
 <!-- /.container-fluid -->
 
+<!-- Full Calendar -->
+<link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.css' rel='stylesheet' />
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js'></script>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      timeZone: 'UTC',
+      initialView: 'dayGridMonth',
+      events: 'https://fullcalendar.io/api/demo-feeds/events.json',
+      editable: true,
+      selectable: true
+    });
+
+    calendar.render();
+  });
+</script>
 @endsection
