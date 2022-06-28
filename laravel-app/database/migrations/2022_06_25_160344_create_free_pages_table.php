e901c0fda5e8335a4a6dc3066386ca7130cb8b09
s-watanabe <swata82@gmail.com>
2022-06-28 21:57:19 +0900

    WIP

---
+++ laravel-app/database/migrations/2022_06_25_160344_create_free_pages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFreePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('free_pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('title');
+            $table->timestamps();
+            $table->index('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('free_pages');
+    }
+}
---
+++ laravel-app/database/migrations/2022_06_25_161515_create_navigation_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNavigationMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('navigation_menus', function (Blueprint $table) {
+            $table->string('code')->primary();
+            $table->integer('type');
+            $table->string('name');
+            $table->integer('sort');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navigation_menus');
+    }
+}
