538eb1d1f785cd52e52acf914aab5fd1c22fec28
Shin Watanabe <swatanabe.apps@gmail.com>
2023-12-02 02:42:51 +0900

    wip

--- snswebapp/resources/views/pictures/index.blade.php
+++ snswebapp/resources/views/pictures/index.blade.php
@@ -1,59 +1,66 @@
 @extends('layouts.app')
 @section('content')
+
 <!-- Page Heading -->
-<div class="card mb-3">
+<div class="card">
     <!-- Card Header - Accordion -->
-    <a href="#test" class="d-block card-header p-0" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseCardExample">
+    <a href="#expand" class="d-block card-header p-0" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseCardExample">
         <nav aria-label="breadcrumb" class="col-md-12 h6 font-weight-bold p-0 m-0">
             <ol class="breadcrumb mb-0">
                 <li class="breadcrumb-item active" aria-current="page"><i class="fas fa-fw fa-images"></i> @lang('strings.pictures')</li>
             </ol>
         </nav>
     </a>
+    
     <!-- Card Content - Collapse -->
-    <div class="collapse show" id="test">
-        <div class="row card-body py-2">
-            <div class="col-lg-6 col-12">
-                {{Form::open([
-                    'name' => 'picturesSearch',
-                    'url' => '/pictures',
-                    'method' => 'get',
-                ])}}
+    <div class="collapse show" id="expand">
+        <div class="d-block card-body p-0">
+            <table class="table responsive-table">
+                <tbody>
+                    <tr>
+                        <td style="width: 50%">
+                            {{Form::open([
+                                'name' => 'picturesSearch',
+                                'url' => '/pictures',
+                                'method' => 'get',
+                            ])}}
 
-                <div class="row py-3">
-                    <div class="col-md-3 col-3">
-                        <span>@lang('strings.title')</span>
-                    </div>
-                    <div class="col-md-9 col-9 pb-2">
-                        {{Form::input('text', 'title', old('title'), [
-                            'style' => 'width: 100%;'
-                        ])}}
-                        <div class="text-danger">{{$errors->first('title') ?? ''}}</div>
-                    </div>
-                    <div class="col-md-3 col-3">
-                        <span>@lang('strings.contributor')</span>
-                    </div>
-                    <div class="col-md-9 col-9 pb-3">
-                        <select name="user_id" class="btn dropdown-toggle pr-1 border text-left w-100">
-                            <option value="0"></option>
-                            @foreach ($users as $user)
-                            <option value="{{$user->id}}">{{$user->name}}</option>
-                            @endforeach
-                        </select>
-                        <div class="text-danger">{{$errors->first('user_id') ?? ''}}</div>
-                    </div>
-                    <div class="col-12 text-center">
-                        <a href="javascript:picturesSearch.submit()" class="btn btn-primary shadow-sm w-25">
-                            <i class="fas fa-search fa-sm"></i> @lang('strings.search')
-                        </a>
-                    </div>
-                </div>
-                {{Form::close()}}
-
-            </div>
-            <div class="col-lg-6 col-12">
-                TODO
-            </div>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 col-3">
+                                    <span>@lang('strings.title')</span>
+                                </div>
+                                <div class="col-lg-9 col-md-8 col-9 pb-2">
+                                    {{Form::input('text', 'title', old('title'), [
+                                        'style' => 'width: 100%;'
+                                    ])}}
+                                    <div class="text-danger">{{$errors->first('title') ?? ''}}</div>
+                                </div>
+                                <div class="col-lg-3 col-md-4 col-3">
+                                    <span>@lang('strings.contributor')</span>
+                                </div>
+                                <div class="col-lg-9 col-md-8 col-9 pb-3">
+                                    <select name="user_id" class="btn dropdown-toggle bg-white pr-1 border text-left w-100">
+                                        <option value="0"></option>
+                                        @foreach ($users as $user)
+                                        <option value="{{$user->id}}">{{$user->name}}</option>
+                                        @endforeach
+                                    </select>
+                                    <div class="text-danger">{{$errors->first('user_id') ?? ''}}</div>
+                                </div>
+                                <div class="col-12 text-center">
+                                    <a href="javascript:picturesSearch.submit()" class="btn btn-primary shadow-sm w-25">
+                                        <i class="fas fa-search fa-sm"></i> @lang('strings.search')
+                                    </a>
+                                </div>
+                            </div>
+                            {{Form::close()}}
+                        </td>
+                        <td style="width: 50%">
+                            TODO
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
