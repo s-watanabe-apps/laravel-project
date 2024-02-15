bbc822a3ad76a5f5d27492718ca8ae5e37d3fcda
Shin Watanabe <swatanabe.apps@gmail.com>
2024-02-15 22:41:59 +0900

    wip

--- snswebapp/resources/views/managements/informations/formset/editor.blade.php
+++ snswebapp/resources/views/managements/informations/formset/editor.blade.php
@@ -48,12 +48,12 @@ editor = CKEDITOR.replace('editor', {
     uiColor: '#eeeeee',
     height: 400,
     disableNativeSpellChecker: true,
-    removeButtons: 'Save,NewPage,About',
+    removeButtons: 'Save,NewPage,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,CreateDiv,BidiLtr,BidiRtl,Language,Flash,Iframe',
     colorButton_backStyle: {
         element: 'span',
         styles: { 'background-color': '#(color)' }
     },
-    colorButton_colors: 'FontColor1/FF9900,FontColor2/0066CC,FontColor3/F00',
+    colorButton_colors: '00f,f00,080',
 });
 
 editor.name = 'body';
